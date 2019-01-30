4940be0d678df1849ab26c928508985a1c753864
Yaltha <rullistyaltha@gmail.com>
2019-01-30 15:47:40 +0700

    add sampai form search ID

--- shopKetti/server.js
+++ shopKetti/server.js
@@ -12,7 +12,7 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
